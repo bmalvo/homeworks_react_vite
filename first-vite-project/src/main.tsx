581d6e697f9834ac0f26e5e929250b041c56b72a
bmalvo <boydmalvo@gmail.com>
2024-02-21 18:43:27 +0100

    create h1 wit sum two variables, create div with children h1 contains name and create h1 with name from prompt

--- first-vite-project/src/main.tsx
+++ first-vite-project/src/main.tsx
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+const first = 33;
+const second = 66;
+
+const myFunc = (name: string) => {
+  return <h1> My name is {name}!</h1>
+};
+
+const yourName = prompt('What is your name?');
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  <div>
+    
+  <h1>Sum elements first and second: {first + second}</h1>
+    <div>{myFunc('Patryk')}</div>
+    <h1>Welcome { yourName }!</h1>
+  </div>
 )
