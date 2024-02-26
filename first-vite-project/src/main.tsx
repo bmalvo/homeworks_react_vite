48e4397ca2871ec72ce3b2dedcdf76e8bc052627
bmalvo <boydmalvo@gmail.com>
2024-02-26 20:19:38 +0100

    task 2

--- first-vite-project/src/main.tsx
+++ first-vite-project/src/main.tsx
@@ -5,22 +5,53 @@ import './index.css'
 const first = 33;
 const second = 66;
 
+
+
+const color = () => {
+  const colors = ['red', 'blue', 'green', 'yellow', 'purple'];
+  return colors[Math.floor(Math.random() * 5)]
+}
+
 const myFunc = (name: string) => {
   return <h1> My name is {name}!</h1>
 };
 
-const yourName = prompt('What is your name?');
+let backDivColor = ''
+
+const promptColor = () => {
+  const color = prompt('Give me a color: ');
+  if (color?.length === 7) {
+    return color;
+  } else {
+    backDivColor = 'black'
+    return 'black';
+  }
+}
+
+// const yourName = prompt('What is your name?');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-<React.StrictMode>
+  <React.StrictMode>
     <div>
 
       <p>First task:</p>
       <h1>Sum elements first and second: {first + second}</h1>
       <div>{myFunc('Patryk')}</div>
-      <h1>Welcome {yourName}!</h1>
+      {/* <h1>Welcome {yourName}!</h1> */}
       
+      <p>task 2:</p>
+      <div className="myDiv" style={
+        {
+          backgroundColor: `${color()}`,
+        }
+      }></div>
       
+      <div className='promptDiv' style={{
+        width: 100,
+        height: 100,
+        backgroundColor: `${promptColor()}`
+      }}>{(backDivColor === 'black')? <p>Wrong color!</p> : '' }</div>
+
   </div>
   </React.StrictMode>,
 )
