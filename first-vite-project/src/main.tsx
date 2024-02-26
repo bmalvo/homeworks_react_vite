51a2a196e6ed2d43dc8995ba1eae111a5def48d8
bmalvo <boydmalvo@gmail.com>
2024-02-26 18:34:53 +0100

    refactor task1

--- first-vite-project/src/main.tsx
+++ first-vite-project/src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 
 const first = 33;
@@ -13,13 +12,15 @@ const myFunc = (name: string) => {
 const yourName = prompt('What is your name?');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-  <div>
-    
-  <h1>Sum elements first and second: {first + second}</h1>
-    <div>{myFunc('Patryk')}</div>
-    <h1>Welcome { yourName }!</h1>
+<React.StrictMode>
+    <div>
+
+      <p>First task:</p>
+      <h1>Sum elements first and second: {first + second}</h1>
+      <div>{myFunc('Patryk')}</div>
+      <h1>Welcome {yourName}!</h1>
+      
+      
   </div>
+  </React.StrictMode>,
 )
