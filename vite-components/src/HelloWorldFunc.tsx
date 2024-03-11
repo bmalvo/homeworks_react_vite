1d47b14e6fc579842e7090de4ced7911a80dafef
bmalvo <boydmalvo@gmail.com>
2024-03-11 19:55:54 +0100

    create components by function

---
+++ vite-components/src/HelloWorldFunc.tsx
@@ -0,0 +1,10 @@
+import { LoremIpsumFunc } from "./LoremIpsumFunc";
+
+const HelloWorldFunc = () => {
+    return <>
+        <h1>Hello World by func!</h1>
+        <LoremIpsumFunc />
+    </>
+}
+
+export { HelloWorldFunc };
---
+++ vite-components/src/LoremIpsumFunc.tsx
@@ -0,0 +1,5 @@
+const LoremIpsumFunc = () => {
+    return <h2>Lorem Ipsum by func</h2>
+}
+
+export { LoremIpsumFunc };
