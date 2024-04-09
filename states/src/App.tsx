a3d377457606dfcb3dd2208079b516c4c75ffe08
bmalvo <boydmalvo@gmail.com>
2024-04-09 21:03:01 +0200

    create states -> first usestate usage

---
+++ states/src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    setCounter(prevCounter => prevCounter + 1)
+  }
+    
+  const reset = () => {
+      setCounter(0);
+  }
+
+  return (<>
+    <h1>{ counter}</h1>
+    <button onClick={increment}>+1</button>
+    <button onClick={reset}>Reset</button>
+  </>)
+}
+
+export { App }
