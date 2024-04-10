d66088021b43863e19a19e6c58c00cecb2c6bb83
bmalvo <boydmalvo@gmail.com>
2024-04-10 19:42:42 +0200

    add Exercise2 - > creating random boxes

--- states-exercises/src/App.tsx
+++ states-exercises/src/App.tsx
@@ -1,7 +1,14 @@
 import { Exercise1 } from "./Exercise1"
+import { Exercise2 } from "./Exercise2"
 
 const App = () => {
-  return <Exercise1 />
+  return (
+    <>
+    <Exercise1 />
+    <Exercise2 />
+    </>
+  )
+
 }
 
 export { App }
---
+++ states-exercises/src/Exercise2.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+type SingleBox = {
+    size: number;
+    x: number;
+    y: number;
+    rotation: number;
+    color: string;
+    borderRadius: number;
+}
+
+export const Exercise2 = () => {
+    const [boxes, setBoxes] = useState<SingleBox[]>([]);
+
+    const addBox = () => {
+        const x = Math.round(Math.random() * 500);
+        const y = Math.round(Math.random() * 500);
+        const size = Math.round(Math.random() * 100);
+        const rotation = Math.round(Math.random() * 360);
+        const borderRadius = Math.round(Math.random() * 50);
+        const r = Math.round(Math.random() * 255);
+        const g = Math.round(Math.random() * 255);
+        const b = Math.round(Math.random() * 255);
+
+        const newBox: SingleBox = {
+            x,
+            y,
+            size,
+            rotation,
+            borderRadius,
+            color: `rgb(${r}, ${g}, ${b})`
+        }
+
+        setBoxes(prevBoxes => [...prevBoxes, newBox]);
+    }
+
+    return (
+        <>
+            <button onClick={addBox}>add random box</button>
+            <div style={{
+                position: 'relative',
+                width: 1000,
+                height: 1000
+            }}>
+                {boxes.map((box, index) => (
+                    <div
+                        key={index}
+                        style={{
+                            backgroundColor: box.color,
+                            width: box.size,
+                            height: box.size,
+                            borderRadius: `${box.borderRadius}%`,
+                            position: 'absolute',
+                            top: box.y,
+                            left: box.x,
+                            transform: `translate(-50%) rotate(${box.rotation}deg)`,
+                            transformOrigin: '50%'
+                        }}/>)
+                )}
+            </div>
+        </>
+    )
+}
