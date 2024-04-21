2f6961c4c52995dfd74f2aa4fc62b7e8f5d19664
bmalvo <boydmalvo@gmail.com>
2024-04-21 17:45:08 +0200

    add ClassLifecycle.tsx

---
+++ efects/src/ClassLifecycle.tsx
@@ -0,0 +1,43 @@
+import { Component } from "react";
+
+type State = {
+    counter: number;
+}
+
+type Props = {}
+
+class ClassLifecycle extends Component<Props, State> {
+    state = {
+        counter: 0,
+    }
+
+    interval: number | null = null;
+
+    componentDidMount(): void {
+        console.log('have been mounted');
+        this.interval = setInterval(() => {
+            this.setState(prevState => ({
+                counter: prevState.counter + 1
+            }))
+        }, 1000)
+    }
+
+    componentDidUpdate(_: Props, prevState: Readonly<State>): void {
+        console.log('Preview counter', prevState.counter);
+        console.log('Current counter', this.state.counter);
+    }
+
+    componentWillUnmount(): void {
+        if (this.interval === null) return;
+
+        clearInterval(this.interval);   
+    }
+
+    render() {
+        return (
+            <h1>{ this.state.counter}</h1>
+        )
+    }
+}
+
+export { ClassLifecycle }
--- efects/src/main.tsx
+++ efects/src/main.tsx
@@ -5,12 +5,14 @@ import './index.css'
 import { IncorrectEffect } from './IncorrectEffect.tsx'
 import { Correct } from './Correct.tsx'
 import { Complicated } from './Compicated.tsx'
+import { ClassLifecycle } from './ClassLifecycle.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
   // <App />
   // <IncorrectEffect />
   // <Correct />
-  <Complicated />
+  // <Complicated />
+  <ClassLifecycle />
   // </React.StrictMode>,
 )
