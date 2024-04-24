c2f719ec56ee481d95c0cbd9aae6f909a547e8f9
bmalvo <boydmalvo@gmail.com>
2024-04-24 20:04:28 +0200

    create App.tsx

--- strictMode/src/App.tsx
+++ strictMode/src/App.tsx
@@ -1,3 +1,17 @@
+import { useEffect, useState } from "react"
+
 export const App = () => {
-  return null
+    const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCounter(prevCounter => prevCounter + 1)
+        }, 1000)
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, [])
+
+    return <h1>{ counter }</h1>
 }
