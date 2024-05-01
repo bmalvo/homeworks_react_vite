233463d377bacd9024b708aa61353416d513de30
bmalvo <boydmalvo@gmail.com>
2024-05-01 20:59:19 +0200

    conditional rendering whole component > fail first

--- conditional-rendering/src/SimpleAgeVerifier.tsx
+++ conditional-rendering/src/SimpleAgeVerifier.tsx
@@ -2,9 +2,23 @@ type SimpleAgeVerifierProps = {
     age: number;
 }
 
-export const SimpleAgeVerifier = ({age}: SimpleAgeVerifierProps) => {
+// # conditional rendering text
+
+// export const SimpleAgeVerifier = ({age}: SimpleAgeVerifierProps) => {
+//     return <div>
+//         <h1>Hi!</h1>
+//         {age >= 18 && 'You are adult!'}
+//   </div>
+// }
+
+// conditional rendering whole component > fail first
+
+export const SimpleAgeVerifier = ({ age }: SimpleAgeVerifierProps) => {
+    
+    if (age < 18) return;
+
     return <div>
         <h1>Hi!</h1>
-        {age >= 18 && 'You are adult!'}
+        <p>You are adult!</p>
   </div>
 }
