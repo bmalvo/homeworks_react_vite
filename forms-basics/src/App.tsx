57465b0f2e45f5a6fa37cec918cf1a1d1b0182bb
bmalvo <boydmalvo@gmail.com>
2024-05-29 22:14:52 +0200

    basic form

--- forms-basics/src/App.tsx
+++ forms-basics/src/App.tsx
@@ -1,6 +1,19 @@
-export const App = () => {
+import { FormEvent } from "react"
 
-  return <form>
-    <input type="text" name="login"/>
+type formData = {
+  login: { value: string };
+}
+
+export const App = () => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { login } = e.target as typeof e.target & formData;
+    console.log(login.value)
+
+  }
+
+  return <form onSubmit={handleSubmit}>
+    <input type="text" name="login" />
+    <button type="submit">Save</button>
   </form>
 }
