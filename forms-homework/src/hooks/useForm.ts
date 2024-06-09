b885d28581026bfbf05dee1d33b3e5842fb5d64d
bmalvo <boydmalvo@gmail.com>
2024-06-09 15:00:15 +0200

    add hook -> useForm.ts

---
+++ forms-homework/src/hooks/useForm.ts
@@ -0,0 +1,31 @@
+import { ChangeEvent, useState } from "react";
+
+type FormChangeEvent = ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>;
+
+type UseFormReturn<T> = [T, (e: FormChangeEvent) => void];
+
+const isCheckboxElement = (target: FormChangeEvent['target']): target is HTMLInputElement => {
+    return target.type === 'checkbox' &&  target instanceof HTMLInputElement;
+}
+
+export const useForm = <T>(initialValues: T): UseFormReturn<T> => {
+    const [formState, setFormState] = useState<T>(initialValues);
+
+    const getValue = (target: FormChangeEvent['target']) => {
+        if (isCheckboxElement(target)) {
+            return target.checked;
+        }
+
+        if (target.type === 'number') return Number(target.value);
+
+        return target.value;
+    }
+
+    const handleChange = (e: FormChangeEvent) => {
+        setFormState(prevState => ({
+            ...prevState,
+            [e.target.name]: getValue(e.target)
+        }))
+    }
+    return [formState, handleChange];
+}
