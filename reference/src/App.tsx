6d13d82ff5e034e19ba9b37d8bd3c328e251c869
bmalvo <boydmalvo@gmail.com>
2024-06-12 19:00:04 +0200

    add Input.tsx

--- reference/src/App.tsx
+++ reference/src/App.tsx
@@ -1,4 +1,23 @@
+import { useRef } from "react";
+import { Input } from "./Input";
+import { ForwarderRefInput } from "./ForwardedRefInput";
+
 export const App = () => {
 
-  return null;
+  const inputRef = useRef<HTMLInputElement>(null);
+  const inputSecondRef = useRef<HTMLInputElement>(null);
+  const inputThirdRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+
+    inputRef.current?.focus();
+
+  }
+
+  return <>
+    <button onClick={handleClick}>Focus</button>
+    <input type="text" ref={inputRef} />
+    <Input passedRef={inputSecondRef} value="Lorem Ipsum" />
+    <ForwarderRefInput value="Lorem Polem" ref={ inputThirdRef} />
+  </>
 }
