b4c8f730f0a754e360548a028bbffc4f42475540
bmalvo <boydmalvo@gmail.com>
2024-06-19 13:22:20 +0200

    using useMemo

--- excessive-rendering/src/App.tsx
+++ excessive-rendering/src/App.tsx
@@ -1,18 +1,25 @@
-import { useRef, useState } from "react"
+import { useMemo, useState } from "react"
 import { Child } from "./Child";
 
 export const App = () => {
 
   const [counter, setCounter] = useState(0);
-  const arr = useRef([5]);
+  const [are, setArr] = useState([5]);
 
   const inc = () => {
 
     setCounter(prevCounter => prevCounter + 1);
   }
 
+  // const arr = useMemo(() => [5], []);
+
+  const addToArray = () => {
+    setArr(prevArr => [...prevArr, Math.random() * 10])
+  }
+
   return <div>
     <button onClick={inc}>+1</button>
-    <Child value={arr.current} />
+    <button onClick={addToArray}>add to array</button>
+    <Child value={arr} counter={counter} />
   </div>
 }
--- excessive-rendering/src/Child.tsx
+++ excessive-rendering/src/Child.tsx
@@ -1,19 +1,24 @@
-import { memo } from "react";
+import { memo, useMemo } from "react";
 
 type ChildProps = {
     value: number[];
+    counter: number; 
 }
 
-const heavyCalculation = () => {
-    console.log('rendering start')
-    for (let i; i < 10000000; i++) {}
+const heavyCalculation = (numbers: number[]) => {
     console.log('rendering stop')
-    return 100
+    let result = numbers.length ** 2;
+    // eslint-disable-next-line no-empty
+    for (let i; i < 100000; i++) {
+        result += (result * 2) % 255; 
+    }
+    console.log('rendering stop')
+    return result
 }
 
-export const Child = memo(({value}: ChildProps) => {
+export const Child = memo(({value, counter}: ChildProps) => {
 
-    const calculatedValue = heavyCalculation();
+    const calculatedValue = useMemo(() => heavyCalculation(value), [value]);
 
     return <h1>Hello, World! {value.join() }{ calculatedValue }</h1>
 })
