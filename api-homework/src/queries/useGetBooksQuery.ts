43d650b8aaa60bba29b3418eebeff629a0343c36
bmalvo <boydmalvo@gmail.com>
2024-07-14 17:09:07 +0200

    create hook useGetBooksQuery.ts

--- api-homework/src/queries/useGetBooksQuery.ts
+++ api-homework/src/queries/useGetBooksQuery.ts
@@ -1,13 +1,15 @@
 import { useQuery } from "@tanstack/react-query"
 import { BookEntity } from "../types";
+import { useApi } from "../hooks/useApi";
 
 export const useGetBooksQuery = () => {
+
+    const { apiGet } = useApi();
 
     const { data } = useQuery<BookEntity[]>({
         queryKey: ['books'],
         queryFn: async () => {
-            const response = await fetch('http://localhost:3000/books');
-            return response.json();
+            return apiGet<BookEntity[]>('books');
         }
     })
 
