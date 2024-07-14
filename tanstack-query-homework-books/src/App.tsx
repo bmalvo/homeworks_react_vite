3e133e5732d38030d2458a32f3e184fa73d15575
bmalvo <boydmalvo@gmail.com>
2024-07-14 13:57:52 +0200

    set QueryClientProvider

--- tanstack-query-homework-books/src/App.tsx
+++ tanstack-query-homework-books/src/App.tsx
@@ -1,4 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { BookList } from "./BookList"
+
+const queryClient = new QueryClient();
+
 export const App = () => {
 
-  return null
+  return <>
+    <QueryClientProvider client={queryClient}>
+      <BookList />
+    </QueryClientProvider>
+  </>
 }
