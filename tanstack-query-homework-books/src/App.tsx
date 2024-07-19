44b92422f4104921fbb51e0f0090e12e1ba1c044
bmalvo <boydmalvo@gmail.com>
2024-07-19 19:59:16 +0200

    add BooksStats.tsx

--- tanstack-query-homework-books/src/App.tsx
+++ tanstack-query-homework-books/src/App.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import {BooksList} from './BooksList'
+import { BookStats } from "./BookStats";
 
 const queryClient = new QueryClient();
 
@@ -7,6 +8,7 @@ export const App = () => {
 
   return <>
     <QueryClientProvider client={queryClient}>
+      <BookStats />
       <BooksList />
     </QueryClientProvider>
   </>
