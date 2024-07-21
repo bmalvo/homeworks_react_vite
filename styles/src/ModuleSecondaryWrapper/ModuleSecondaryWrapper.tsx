b3ea1d8e3d6d711f2fe46fa9343bb3b889bea6fd
bmalvo <boydmalvo@gmail.com>
2024-07-21 20:21:15 +0200

    conditional switching styles

--- styles/src/ModuleSecondaryWrapper/ModuleSecondaryWrapper.tsx
+++ styles/src/ModuleSecondaryWrapper/ModuleSecondaryWrapper.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react"
+import { ReactNode, useState } from "react"
 import styles from './ModuleSecondaryWrapper.module.scss'
 
 type ModuleSecondaryWrapperProps = {
@@ -6,10 +6,17 @@ type ModuleSecondaryWrapperProps = {
     children: ReactNode
 }
 
-export const ModuleSecondaryWrapper = ({children}: ModuleSecondaryWrapperProps) => {
+export const ModuleSecondaryWrapper = ({ children }: ModuleSecondaryWrapperProps) => {
+    
+    const [isActive, setIsActiv] = useState(false);
+
+    const toggleActive = () => {
+
+        setIsActiv(prevActiv => !prevActiv)
+    }
 
     return (
-        <div className={styles.wrapper}>
+        <div className={`${styles.wrapper} ${isActive? styles.active : null}`} onClick={toggleActive} role="button">
             {children}
         </div>
     )
