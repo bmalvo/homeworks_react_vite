b741da455dc7b0a83c86ac4927fb96b607b05bfe
bmalvo <boydmalvo@gmail.com>
2024-07-30 12:49:45 +0200

    Module wrapper and moduleSecondaryWrapper

--- styles/src/ModuleSecondaryWrapper/ModuleSecondaryWrapper.tsx
+++ styles/src/ModuleSecondaryWrapper/ModuleSecondaryWrapper.tsx
@@ -9,15 +9,24 @@ type ModuleSecondaryWrapperProps = {
 export const ModuleSecondaryWrapper = ({ children }: ModuleSecondaryWrapperProps) => {
     
     const [isActive, setIsActiv] = useState(false);
+    const [isPrimary, setPrimary] = useState(false);
 
     const toggleActive = () => {
 
         setIsActiv(prevActiv => !prevActiv)
     }
 
+    const togglePrimary = () => {
+
+        setPrimary(prevPrimary => !prevPrimary)
+    }
+
     return (
-        <div className={`${styles.wrapper} ${isActive? styles.active : null}`} onClick={toggleActive} role="button">
+        <>
+            <button onClick={togglePrimary}>toggle primary</button>
+        <div className={`${styles.wrapper} ${isActive? styles.active : ''} ${isPrimary? styles.primary : ''}`} onClick={toggleActive} role="button">
             {children}
         </div>
+        </>
     )
 }
