a9d9bf7d8e72d2262bfbab6c8d84744c7a1f7590
bmalvo <boydmalvo@gmail.com>
2024-08-02 17:06:12 +0200

    create babel-plugin-styled-components

---
+++ styles-babel/src/Foo.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+
+    padding: 30px;
+    color: #333;
+    background-color: #eee`
+
+export const Foo = () => {
+
+    return <Wrapper>Lorem ipsum dolor sit amet.</Wrapper>
+}
