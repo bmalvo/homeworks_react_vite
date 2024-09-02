07f8d7d385c66886ca49e07cd9626e52e89ae612
bmalvo <boydmalvo@gmail.com>
2024-09-02 18:21:59 +0200

    use property of index

--- react-router/src/App.tsx
+++ react-router/src/App.tsx
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
     element: <About />,
     children: [
       {
-        path: 'us',
+        index: true,
         element: <AboutUs />
       },
       {
@@ -35,17 +35,17 @@ const router = createBrowserRouter([
 export const App = () => {
 
   return (
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path='/' element={ <Home />} />
-    //     <Route path='/hello' element={<Hello />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <Home />} />
+        <Route path='/hello' element={<Hello />} />
         
-    //     <Route path='about' element={<About />}>
-    //     <Route path='us' element={ <AboutUs />} />
-    //     <Route path='company' element={ <AboutCompany />} />
-    //     </Route>
-    //   </Routes>
-    // </BrowserRouter>
-    <RouterProvider router={router} />
+        <Route path='about' element={<About />}>
+        <Route index element={ <AboutUs />} />
+        <Route path='company' element={ <AboutCompany />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    // <RouterProvider router={router} />
   )
 }
