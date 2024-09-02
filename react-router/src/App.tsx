195f394a0df4383d7776c9e71eca3f58362a13d9
bmalvo <boydmalvo@gmail.com>
2024-09-02 17:48:46 +0200

    create browser router with createBrowserRouter

--- react-router/src/App.tsx
+++ react-router/src/App.tsx
@@ -1,13 +1,22 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes } from "react-router-dom"
 import { Hello } from "./Hello"
+
+const router = createBrowserRouter([
+
+  {
+    path: '/',
+    element: <Hello />
+  }
+])
 
 export const App = () => {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={ <Hello />} />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path='/' element={ <Hello />} />
+    //   </Routes>
+    // </BrowserRouter>
+    <RouterProvider router={router} />
   )
 }
