54db1682d2c09529e5b2b20dfbf4a1224ec9fcca
bmalvo <boydmalvo@gmail.com>
2024-09-11 12:29:12 +0200

    improve Errorelement

--- react-router/src/ErrorElement.tsx
+++ react-router/src/ErrorElement.tsx
@@ -1,11 +1,25 @@
-import { useRouteError } from "react-router-dom"
+import { useRouteError, isRouteErrorResponse } from "react-router-dom"
 
 export const ErrorElement = () => {
 
-    const error = useRouteError() as string;
+    const error = useRouteError();
+
+    const formatError = (appError: unknown) => {
+
+        if (appError === 'string') return appError;
+
+        if (isRouteErrorResponse(appError)) {
+            console.log(appError.data)
+        }
+
+        if (appError instanceof Error) {
+
+            return appError.message;
+        }
+    }
 
     return <div>
         <h1>Error!</h1>
-        <p>something went wrong... { error}</p>
+        <p>something went wrong... { formatError(error)}</p>
     </div>
 }
