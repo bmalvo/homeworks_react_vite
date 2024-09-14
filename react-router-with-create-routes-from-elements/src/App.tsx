7c2bdeff2da52af028c3e8362b0ed680335a205e
bmalvo <boydmalvo@gmail.com>
2024-09-14 21:40:08 +0200

    using createRoutesFromElements

--- react-router-with-create-routes-from-elements/src/App.tsx
+++ react-router-with-create-routes-from-elements/src/App.tsx
@@ -1,9 +1,9 @@
-//  1- classic way
-
-import { BrowserRouter, createBrowserRouter, Navigate, Route, RouterProvider, Routes } from "react-router-dom"
+import { BrowserRouter, createBrowserRouter, createRoutesFromElements, Navigate, Route, RouterProvider, Routes } from "react-router-dom"
 import { MainLayout } from "./MainLayout"
 import { Account } from "./Account"
 import { About } from "./About"
+
+//  1- classic way
 
 // export const App = () => {
 
@@ -22,35 +22,62 @@ import { About } from "./About"
 
 // 2- using creatBrowserRouter
 
-const router = createBrowserRouter([
+// const router = createBrowserRouter([
 
-  {
-    element: <MainLayout />,
-    children: [
+//   {
+//     element: <MainLayout />,
+//     children: [
 
-      {
-        path: 'account',
-        element: <Account />,
-        loader: () => {
-          console.log('loader is running')
-          return 'lorem ipsum'
-        }
-      },
-      {
-        path: 'about',
-        element: <About />
-      },
-      {
-        path: '*',
-        element: <Navigate to='account' />
-      }
-    ]
-  }
-])
+//       {
+//         path: 'account',
+//         element: <Account />,
+//         loader: () => {
+//           console.log('loader is running')
+//           return 'lorem ipsum'
+//         }
+//       },
+//       {
+//         path: 'about',
+//         element: <About />
+//       },
+//       {
+//         path: '*',
+//         element: <Navigate to='account' />
+//       }
+//     ]
+//   }
+// ])
+
+// export const App = () => {
+
+//   return <>
+//     <RouterProvider router={ router} />
+//   </>
+// }
+
+//  using createRoutesFromElements
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route element={<MainLayout />}>
+    <Route
+      path='account'
+      element={<Account />}
+      loader={() => {
+        
+        return 'lorem ipsum'
+      }} />
+    <Route
+      path='about'
+      element={<About />} />
+    <Route
+      path='*'
+      element={<Navigate to='account' />} />
+  </Route>
+));
 
 export const App = () => {
 
   return <>
-    <RouterProvider router={ router} />
+    <RouterProvider router={router} />
   </>
 }
