5fd7a39e4e0163bae229f11dfe5a8eb58155841b
bmalvo <boydmalvo@gmail.com>
2024-09-14 21:30:42 +0200

    using createBrowserRouter

--- react-router-with-create-routes-from-elements/src/App.tsx
+++ react-router-with-create-routes-from-elements/src/App.tsx
@@ -1,21 +1,56 @@
 //  1- classic way
 
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import { BrowserRouter, createBrowserRouter, Navigate, Route, RouterProvider, Routes } from "react-router-dom"
 import { MainLayout } from "./MainLayout"
 import { Account } from "./Account"
 import { About } from "./About"
 
+// export const App = () => {
+
+//   return <>
+//     <BrowserRouter >
+//       <Routes>
+//         <Route element={<MainLayout />}>
+//           <Route path='account' element={<Account />} />
+//           <Route path='about' element={<About />} />
+//           <Route path='*' element={<Navigate to='account' />} />
+//         </Route>
+//       </Routes>
+//     </BrowserRouter>
+//   </>
+// }
+
+// 2- using creatBrowserRouter
+
+const router = createBrowserRouter([
+
+  {
+    element: <MainLayout />,
+    children: [
+
+      {
+        path: 'account',
+        element: <Account />,
+        loader: () => {
+          console.log('loader is running')
+          return 'lorem ipsum'
+        }
+      },
+      {
+        path: 'about',
+        element: <About />
+      },
+      {
+        path: '*',
+        element: <Navigate to='account' />
+      }
+    ]
+  }
+])
+
 export const App = () => {
 
   return <>
-    <BrowserRouter >
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path='account' element={<Account />} />
-          <Route path='about' element={<About />} />
-          <Route path='*' element={<Navigate to='account' />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={ router} />
   </>
 }
