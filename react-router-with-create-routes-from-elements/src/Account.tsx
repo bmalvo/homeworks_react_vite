4fc41b754bd8fab7904cbbd112ad4058aa2f4a5b
bmalvo <boydmalvo@gmail.com>
2024-09-15 11:45:27 +0200

    using fetcher

--- react-router-with-create-routes-from-elements/src/Account.tsx
+++ react-router-with-create-routes-from-elements/src/Account.tsx
@@ -1,8 +1,11 @@
-import { Form, useSubmit } from "react-router-dom"
+import { useEffect } from "react";
+import { Form, useFetcher, useSubmit } from "react-router-dom"
 
 export const Account = () => {
 
     const submit = useSubmit();
+
+    const fetcher = useFetcher();
 
     const handleSubmit = () => {
 
@@ -15,12 +18,30 @@ export const Account = () => {
         })
     }
 
+    useEffect(() => {
+        const data = fetcher.data;
+
+        if (!data) return;
+        console.log('DATA', fetcher.data);
+    }, [fetcher])
+    
+    useEffect(() => {
+
+        fetcher.load('/about')
+    }, [])
+
+    console.log(fetcher);
+
     return <>
         <h1>Account</h1>
         <Form method="post" action="/create">
             <input type="text" name="title" />
             <button type="submit">Create</button>
         </Form>
+        <fetcher.Form method="post" action="/create">
+            <input type="text" name="title" />
+            <button type="submit">Create by fetcher</button>
+        </fetcher.Form>
         <button type="button" onClick={handleSubmit}>Submit with useSubmit</button>
     </>
 }
