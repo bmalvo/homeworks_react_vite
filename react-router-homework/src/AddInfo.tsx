44db553e3d73666f4783b2a01fb0488fd437ddff
bmalvo <boydmalvo@gmail.com>
2024-09-19 19:53:30 +0200

    handle tittle to addInfo added

--- react-router-homework/src/AddInfo.tsx
+++ react-router-homework/src/AddInfo.tsx
@@ -1,14 +1,24 @@
-import { Form } from "react-router-dom"
+import { Form, useActionData } from "react-router-dom"
+import { SingleInfo } from "./types"
+import { useEffect, useState } from "react";
 
 export const AddInfo = () => {
+
+    const data = useActionData() as SingleInfo | undefined;
+    const [title, setTitle] = useState('new title');
+
+    useEffect(() => {
+        setTitle('new title');
+    },[data])
 
     return <>
         <div>
             <h1>Create ne info</h1>
             <Form method="POST">
-                <input type="text" name="title" />
+                <input type="text" name="title" value={title} onChange={e => setTitle(e.target.value)}/>
                 <button type="submit">add info</button>
             </Form>
+            {data ? <p>info { data.title} has been added</p>: null}
         </div>
     </>
 }
