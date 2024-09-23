ff68cafcdfdc9f06d62b4ff65e91867d4771aa2a
bmalvo <boydmalvo@gmail.com>
2024-09-23 13:30:45 +0200

    add post.tsx

---
+++ TanStack-Router/src/routes/post.tsx
@@ -0,0 +1,13 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+const Post = () => {
+
+  return <>
+    <h2>Posts</h2>
+    <p>Lorem ipsum dolor sit amet consectetur.</p>
+  </>
+}
+
+export const Route = createFileRoute('/post')({
+  component: Post,
+})
