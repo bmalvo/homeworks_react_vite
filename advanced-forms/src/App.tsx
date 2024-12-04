83db254f20898a1e9b8e4d085d8f304e9266d9ba
bmalvo <boydmalvo@gmail.com>
2024-12-04 21:07:36 +0100

    ...watch

--- advanced-forms/src/App.tsx
+++ advanced-forms/src/App.tsx
@@ -5,11 +5,12 @@ type FormData = {
 
   login: string;
   password: string;
+  agreement: boolean;
 }
 
 export const App = () => {
 
-  const { register, handleSubmit, reset, getValues, setValue,  formState: {isValid, errors} } = useForm<FormData>();
+  const { register, handleSubmit, reset, getValues, setValue, watch,  formState: {isValid, errors} } = useForm<FormData>();
   
   const onSubmit = (data: FormData) => {
 
@@ -32,7 +33,11 @@ export const App = () => {
   useEffect(() => {
 
     setValue('login', 'Brygida')
-  },[]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  
+  const watching = watch(['login', 'password'])
+  console.log(watching);
 
   return <>
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -40,6 +45,7 @@ export const App = () => {
       {errors.login && errors.login.type === 'required' ? <p>Login is required!</p> : null}
       {errors.login && errors.login.type === 'minLength' ? <p>Min 3 characters in login</p> : null}
       <input type="password" {...register('password')} />
+      <input type="checkbox" {...register('agreement')} />
       <button type="button" onClick={resetForm}>Reset</button>
       <button type="button" onClick={readValues}>Get Values</button>
       <button type="submit">Log in</button>
