0f23cf68f9214a70ace61801073b1421a5306687
bmalvo <boydmalvo@gmail.com>
2024-12-07 10:14:28 +0100

    validation with Dto

--- advanced-forms-validation/src/App.tsx
+++ advanced-forms-validation/src/App.tsx
@@ -1,22 +1,28 @@
-import { InferType, object, string, ValidationError } from "yup";
+import { InferType, object, ObjectSchema, string, ValidationError } from "yup";
 
-const loginSchema = object({
+type loginDto = {
+
+  login: string;
+  password: string;
+}
+
+const loginSchema: ObjectSchema<loginDto> = object({
 
   login: string().required().min(5),
   password: string().required().min(5)
 })
+ 
+// type LoginData = InferType<typeof loginSchema>;
 
-type LoginData = InferType<typeof loginSchema>;
-
-try {
-  await loginSchema.validate({
-    login: '',
-    password: '12345'
-  })
-} catch (e) {
-  const error_message = e instanceof ValidationError ? console.log(e.message) : '';
-  console.log(error_message)
-}
+// try {
+//   await loginSchema.validate({
+//     login: '',
+//     password: '12345'
+//   })
+// } catch (e) {
+//   const error_message = e instanceof ValidationError ? console.log(e.message) : '';
+//   console.log(error_message)
+// }
 
 export const App = () => {
 
