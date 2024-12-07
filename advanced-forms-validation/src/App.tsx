661901f966df440543295ed7057c61904ccf98ab
bmalvo <boydmalvo@gmail.com>
2024-12-07 10:11:37 +0100

    simple validation schema using yup

--- advanced-forms-validation/src/App.tsx
+++ advanced-forms-validation/src/App.tsx
@@ -1,3 +1,23 @@
+import { InferType, object, string, ValidationError } from "yup";
+
+const loginSchema = object({
+
+  login: string().required().min(5),
+  password: string().required().min(5)
+})
+
+type LoginData = InferType<typeof loginSchema>;
+
+try {
+  await loginSchema.validate({
+    login: '',
+    password: '12345'
+  })
+} catch (e) {
+  const error_message = e instanceof ValidationError ? console.log(e.message) : '';
+  console.log(error_message)
+}
+
 export const App = () => {
 
   return null;
