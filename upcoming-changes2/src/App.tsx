b06204d18049ab9e9d3e6a9fb88d5b617a22e243
bmalvo <boydmalvo@gmail.com>
2024-12-16 06:58:02 +0100

    useTransition in React 18

--- upcoming-changes2/src/App.tsx
+++ upcoming-changes2/src/App.tsx
@@ -1,6 +1,43 @@
+import { useState, useTransition } from "react"
+
+const getValues = () => {
+
+  const elements: number[] = []; 
+
+  for (let i = 0; i < 10000; i++) {
+    
+    elements.push(Math.round(Math.random() * 10000))
+  }
+
+  return elements;
+}
+
 export const App = () => {
 
+  const [elements, setElements] = useState<number[]>([]);
+  const [counter, setCounter] = useState<number>(0);
+  const [pending, startTransition] = useTransition();
+
+  const generate = () => {
+
+    setCounter(prev => prev + 1);
+    startTransition(() => {
+      setElements(getValues());
+    })
+  }
+
   return <>
-    
+    <div>
+      <button onClick={generate}>set elements</button>
+      <h2>Clicks: { counter}</h2>
+      <ul>
+        {elements.map((el, index) => <li key={index}>
+          <div>
+            <h2>{el}</h2>
+            <p>this is {index + 1} value: { el}</p>
+          </div>
+        </li>)}
+      </ul>
+    </div>
   </>
 }
