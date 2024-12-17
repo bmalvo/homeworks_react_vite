3f1c81a209a6d1a07c5e3048094474956669c3bf
bmalvo <boydmalvo@gmail.com>
2024-12-17 21:32:59 +0100

    create navigation

---
+++ next/src/app/components/MainNavigation/MainNavigation.tsx
@@ -0,0 +1,25 @@
+'use client';
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import styles from './styles.module.scss'
+
+export const MainNavigation = () => {
+
+    const pathname = usePathname();
+
+    return <>
+        <nav>
+            <ul className={styles.navigation}>
+                <li>
+                    <Link className={pathname.startsWith('/panel') ? styles.active : ''}  href='/panel'>Panel</Link>
+                </li>
+                <li>
+                    <Link className={pathname.startsWith('/admin') ? styles.active : ''}  href='/admin'>Admin</Link>
+                </li>
+                <li>
+                    <Link className={pathname.startsWith('/about') ? styles.active : ''}  href='/about'>About</Link>
+                </li>
+            </ul>
+        </nav>
+    </>
+}
--- next/src/app/layout.tsx
+++ next/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { MainNavigation } from "./components/MainNavigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <header>
+          <MainNavigation />
+        </header>
         {children}
       </body>
     </html>
