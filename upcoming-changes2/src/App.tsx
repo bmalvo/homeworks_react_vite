a4a26789b731c276cb08ac7b658ba82840861d34
bmalvo <boydmalvo@gmail.com>
2024-12-17 09:17:01 +0100

    method use()

--- upcoming-changes2/src/App.tsx
+++ upcoming-changes2/src/App.tsx
@@ -1,14 +1,44 @@
 // import { Form } from "./Form"
 
-import { useState } from "react";
-import { EmailForm } from "./EmailForm"
+import { Suspense, useCallback, useState } from "react"
+import { Use } from "./Use"
+import { ErrorBoundary } from "react-error-boundary";
+
+// import { useState } from "react";
+// import { EmailForm } from "./EmailForm"
 
 export const App = () => {
 
-  const [userEmail, setUserEmail] = useState('email@email.com');
+  const [visible, setVisible] = useState(true);
+
+  const promise = useCallback(() => 
+
+    new Promise<number[]>((resolve, reject) => {
+  
+      setTimeout(() => {
+        const random = Math.random();
+        if (random > 0.5) {
+          
+          resolve([1, 2, 3, 4, 5])
+        } else {
+
+          reject();
+        }
+      }, 3000)
+    }), []);
+
+
+  // const [userEmail, setUserEmail] = useState('email@email.com');
 
   return <>
-    {/* <Form /> */}
-    <EmailForm currentEmail={ userEmail} onEmailUpdate={setUserEmail} />
+    {/* <Form />
+    <EmailForm currentEmail={ userEmail} onEmailUpdate={setUserEmail} /> */}
+
+    <button onClick={() => setVisible(prev => !prev)}>Click</button>
+    <ErrorBoundary fallback={<p>something went wrong...</p>}>
+    <Suspense fallback={<p>loading...</p>}>
+        <Use dataPromise={promise()} visible={ visible} />
+    </Suspense>
+    </ErrorBoundary>
   </>
 }
