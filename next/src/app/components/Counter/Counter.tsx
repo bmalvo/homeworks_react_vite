ac7df8c5aa64a0c969559eb853750cf879cfe239
bmalvo <boydmalvo@gmail.com>
2024-12-17 20:30:16 +0100

    create component Counter with state and use client adnotation

--- next/src/app/components/Counter/Counter.tsx
+++ next/src/app/components/Counter/Counter.tsx
@@ -1,10 +1,20 @@
+'use client';
+import { useState } from 'react'
 import styles from './style.module.scss'
 
 export const Counter = () => {
 
+    const [counter, setCounter] = useState(0);
+
+    const increment = () => {
+
+        setCounter(prev => prev + 1);
+    }
+
     return <>
         <div className={styles.counter}>
-            0
+            {counter}
+            <button onClick={increment}>+1</button>
         </div>
     </>
 }
