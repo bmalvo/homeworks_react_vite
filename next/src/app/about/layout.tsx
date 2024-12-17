31200d7a25bb21107d14cca637f032dfc28b654e
bmalvo <boydmalvo@gmail.com>
2024-12-17 21:37:30 +0100

    navigation with useRouter

--- next/src/app/about/layout.tsx
+++ next/src/app/about/layout.tsx
@@ -1,5 +1,7 @@
+'use client'
 import { ReactNode } from "react";
 import { Container } from "../components/Container";
+import { useRouter } from "next/navigation";
 
 export type AboutLayoutProps = Readonly<{
 
@@ -7,10 +9,22 @@ export type AboutLayoutProps = Readonly<{
 }>
 
 const AboutLayout = ({ children }: AboutLayoutProps) => {
+
+    const router = useRouter();
+
+    const handleClick = () => {
+
+        router.push('/about/me')
+    }
   
     return (
         <Container >
             <h1>About</h1>
+            <ul>
+                <li>
+                    <button onClick={handleClick}>go to about:me</button>
+                </li>
+            </ul>
             <section>
                 {children}
             </section>
