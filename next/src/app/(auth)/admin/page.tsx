a8bbf84298e700ea9a0f63721eedca5552471e39
bmalvo <boydmalvo@gmail.com>
2024-12-17 21:04:43 +0100

    grouping routes

---
+++ next/src/app/(auth)/admin/page.tsx
@@ -0,0 +1,14 @@
+import { Container } from "../../components/Container";
+
+
+const Admin = () => {
+    
+
+    return (
+        <Container>
+            <h2>Admin!</h2>
+        </Container>
+    )
+}
+
+export default Admin;
---
+++ next/src/app/(auth)/panel/page.tsx
@@ -0,0 +1,14 @@
+import { Container } from "../../components/Container";
+
+
+const Panel = () => {
+    
+
+    return (
+        <Container>
+            <h2>Panel!</h2>
+        </Container>
+    )
+}
+
+export default Panel;
