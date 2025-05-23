e9874d5168296324a748e06560ff33057030e94d
bmalvo <boydmalvo@gmail.com>
2025-05-23 22:33:30 +0200

    start first tests

---
+++ testowanie/testing/src/utils/sum.spec.ts
@@ -0,0 +1,14 @@
+import { sum } from "./sum"
+
+describe('sum', () => {
+
+    it('should return correct result for 2 + 2', () => {
+
+        expect(sum(2,2)).toBe(4)
+    })
+
+    it('should retur correct result for add -2 to 2', () => {
+
+        expect(sum(-2,2)).toBe(0)
+    })
+})
